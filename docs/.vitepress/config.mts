6fbfd32d4a4ce78fc99b2311d5e8c3f36d7f2282
14569 <notessentials@163.com>
2025-02-19 10:32:01 +0000

    Initial commit

---
+++ docs/.vitepress/config.mts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Lezi Notes",
+  description: "收集网络沙雕乐子",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '开始', link: '/start' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Lezi Notes 名人堂',
+        items: [
+          { text: '开始阅读', link: '/start' },
+          { text: '自由世界', link: '/zy123' },
+          { text: 'nn2', link: '/nn2' },
+          { text: 'tnt', link: '/tnt' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/lezinote/lezinote.github.io' }
+    ]
+  }
+})
---
+++ docs/.vitepress/theme/GiscusComment.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="comment">
+      <component
+        v-if="isDark"
+        :is="'script'"
+        src="https://giscus.app/client.js"
+        data-repo="lezinote/lezinote.github.io"
+        data-repo-id="R_kgDON65SXw"
+        data-category="Comments"
+        data-category-id="DIC_kwDON65SX84CnGL1"
+        data-mapping="title"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="dark"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        data-loading="eager"
+        async
+      >
+      </component>
+      <component
+        v-else
+        :is="'script'"
+        src="https://giscus.app/client.js"
+        data-repo="lezinote/lezinote.github.io"
+        data-repo-id="R_kgDON65SXw"
+        data-category="Comments"
+        data-category-id="DIC_kwDON65SX84CnGL1"
+        data-mapping="title"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="light"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        data-loading="eager"
+        async
+      >
+      </component>
+    </div>
+  </template>
+  <script setup lang="ts">
+    import { useData } from 'vitepress';
+    const { isDark } = useData();
+  </script>
+  <style lang="scss" scoped>
+    .comment {
+      margin-top: 16px;
+    }
+  </style>
